0a37c12113a192ee2312d62b784b8b2e98be4688
Andrei Lissovski <andrei@lissovski.org>
2016-02-09 17:52:18 +0000

    Resilience test for KafkaActorPublisher-based Source, where Kafka becomes unavailable during processing.

--- src/test/scala/me/crowdmix/reactivekafka/resilience/StreamResilienceSpec.scala
+++ src/test/scala/me/crowdmix/reactivekafka/resilience/StreamResilienceSpec.scala
@@ -51,9 +51,10 @@ class StreamResilienceSpec extends TestKit(ActorSystem("StreamResilienceSpec", C
   with Matchers
   with EmbeddedKafka
   with Eventually
+  with BeforeAndAfter
   with BeforeAndAfterAll {
 
-  implicit val kafkaConfig = EmbeddedKafkaConfig(kafkaPort = 9092, zooKeeperPort = 2181)
+  implicit val kafkaConfig = EmbeddedKafkaConfig()
   val brokerList = s"localhost:${kafkaConfig.kafkaPort}"
   val zooKeeperHost = s"localhost:${kafkaConfig.zooKeeperPort}"
 
@@ -69,6 +70,11 @@ class StreamResilienceSpec extends TestKit(ActorSystem("StreamResilienceSpec", C
         .withSupervisionStrategy(alwaysResume)
     )(system)
 
+  after {
+    EmbeddedKafka.stopKafka()
+    EmbeddedKafka.stopZooKeeper()
+  }
+
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
   }
@@ -77,63 +83,58 @@ class StreamResilienceSpec extends TestKit(ActorSystem("StreamResilienceSpec", C
     "be resilient to Kafka becoming unavailable and continue processing when Kafka comes back up" in {
     val kafka = new ReactiveKafka()
 
-    try {
-      val zkDir = testZooKeeperDir()
-      val kafkaDir = testKafkaDir()
-      EmbeddedKafka.startZooKeeper(zkDir)
-      EmbeddedKafka.startKafka(kafkaDir)
-      // let ZooKeeper/Kafka start up
-      //TODO bake this into EmbeddedKafka itself, fe via heartbeating of some sort
-      Thread.sleep(5000)
+    val zkDir = testZooKeeperDir()
+    val kafkaDir = testKafkaDir()
+    EmbeddedKafka.startZooKeeper(zkDir)
+    EmbeddedKafka.startKafka(kafkaDir)
+    // let ZooKeeper/Kafka start up
+    //TODO bake this into EmbeddedKafka itself, fe via heartbeating of some sort
+    Thread.sleep(5 * 1000)
 
-      val topic = testTopic()
-      val clientId = testClientId()
+    val topic = testTopic()
+    val clientId = testClientId()
 
-      val producerProperties = ProducerProperties(
-        brokerList = brokerList,
-        topic = topic,
-        clientId = clientId,
-        encoder = new StringEncoder)
+    val producerProperties = ProducerProperties(
+      brokerList = brokerList,
+      topic = topic,
+      clientId = clientId,
+      encoder = new StringEncoder)
 
-      val kafkaActorSubscriber = ActorSubscriber[String](
-        system.actorOf(
-          kafka.producerActorProps(producerProperties),
-          testSinkName()))
+    val kafkaActorSubscriber = ActorSubscriber[String](
+      system.actorOf(
+        kafka.producerActorProps(producerProperties),
+        testSinkName()))
 
-      val kafkaSink = Sink(kafkaActorSubscriber)
+    val kafkaSink = Sink(kafkaActorSubscriber)
 
-      val sourceActorRef = Source.actorRef(bufferSize = 100, overflowStrategy = OverflowStrategy.fail)
-        .map { v: String =>
-          v.toUpperCase
-        }
-        .to(kafkaSink)
-        .run()
-
-      sourceActorRef ! "a"
-
-      eventually {
-        consumeFirstStringMessageFrom(topic) shouldBe "A"
+    val sourceActorRef = Source.actorRef(bufferSize = 100, overflowStrategy = OverflowStrategy.fail)
+      .map { v: String =>
+        v.toUpperCase
       }
+      .to(kafkaSink)
+      .run()
 
-      EmbeddedKafka.stopKafka()
+    sourceActorRef ! "a"
 
-      sourceActorRef ! "b"
+    eventually {
+      consumeFirstStringMessageFrom(topic) shouldBe "A"
+    }
 
-      Thread.sleep(3000)  // let Reactive Kafka producer give up retrying before bringing Kafka back up
-      //TODO ascertain [akka://WriteOnlyStreamSpec/user/kafka-sink-UUID] Failed to send messages after X tries.
-      //TODO ascertain [akka://WriteOnlyStreamSpec/user/kafka-sink-UUID] restarted
+    EmbeddedKafka.stopKafka()
 
-      EmbeddedKafka.startKafka(kafkaDir)
-      Thread.sleep(5000)  // let Kafka start up
+    sourceActorRef ! "b"
 
-      sourceActorRef ! "c"
+    Thread.sleep(3 * 1000)  // let Reactive Kafka producer give up retrying before bringing Kafka back up
+    //TODO ascertain [akka://WriteOnlyStreamSpec/user/kafka-sink-UUID] Failed to send messages after X tries.
+    //TODO ascertain [akka://WriteOnlyStreamSpec/user/kafka-sink-UUID] restarted
 
-      eventually {
-        consumeFirstStringMessageFrom(topic) shouldBe "C"
-      }
-    } finally {
-      EmbeddedKafka.stopKafka()
-      EmbeddedKafka.stopZooKeeper()
+    EmbeddedKafka.startKafka(kafkaDir)
+    Thread.sleep(5 * 1000)  // let Kafka start up
+
+    sourceActorRef ! "c"
+
+    eventually {
+      consumeFirstStringMessageFrom(topic) shouldBe "C"
     }
   }
 
@@ -162,7 +163,7 @@ class StreamResilienceSpec extends TestKit(ActorSystem("StreamResilienceSpec", C
       .to(kafkaSink)
       .run()
 
-    Thread.sleep(5000)  // let Reactive Kafka producer give up retrying before starting Kafka
+    Thread.sleep(5 * 1000)  // let Reactive Kafka producer give up retrying before starting Kafka
     //TODO ascertain [akka://WriteOnlyStreamSpec/user/kafka-sink-UUID] Failed to send messages after X tries.
     //TODO ascertain [akka://WriteOnlyStreamSpec/user/kafka-sink-UUID] restarted
 
@@ -177,54 +178,101 @@ class StreamResilienceSpec extends TestKit(ActorSystem("StreamResilienceSpec", C
     "be resilient to Kafka being unavailable initially and begin processing as soon as Kafka becomes available" in {
     val zkDir = testZooKeeperDir()
     val kafkaDir = testKafkaDir()
-    try {
-      EmbeddedKafka.startZooKeeper(zkDir)
-      EmbeddedKafka.startKafka(kafkaDir)
+    EmbeddedKafka.startZooKeeper(zkDir)
+    EmbeddedKafka.startKafka(kafkaDir)
 
-      // let ZooKeeper/Kafka start up - otherwise publishStringMessageToKafka() may throw net.manub.embeddedkafka.KafkaUnavailableException
-      Thread.sleep(10 * 1000)
+    // let ZooKeeper/Kafka start up - otherwise publishStringMessageToKafka() may throw net.manub.embeddedkafka.KafkaUnavailableException
+    Thread.sleep(5 * 1000)
 
-      val topic = testTopic()
+    val topic = testTopic()
 
-      publishStringMessageToKafka(topic, "a")
-      publishStringMessageToKafka(topic, "b")
+    publishStringMessageToKafka(topic, "a")
+    publishStringMessageToKafka(topic, "b")
 
-      EmbeddedKafka.stopKafka()
+    EmbeddedKafka.stopKafka()
 
-      val kafka = new ReactiveKafka()
+    val kafka = new ReactiveKafka()
 
-      val consumerProperties =
-        ConsumerProperties(
-          brokerList = brokerList,
-          zooKeeperHost = zooKeeperHost,
-          topic = topic,
-          groupId = testConsumerGroupId(),
-          decoder = new StringDecoder())
-      val consumerActor = system.actorOf(
-        kafka.consumerActorProps(consumerProperties),
-        testSourceName())
+    val consumerProperties =
+      ConsumerProperties(
+        brokerList = brokerList,
+        zooKeeperHost = zooKeeperHost,
+        topic = topic,
+        groupId = testConsumerGroupId(),
+        decoder = new StringDecoder())
+    val consumerActor = system.actorOf(
+      kafka.consumerActorProps(consumerProperties),
+      testSourceName())
 
-      val sinkProbe =
-        Source(ActorPublisher[KafkaMessage[String]](consumerActor))
-          .map(_.message)
-          .map { v: String =>
-            v.toUpperCase
-          }
-          .toMat(TestSink.probe[String])(Keep.right)
-          .run()
+    val sinkProbe =
+      Source(ActorPublisher[KafkaMessage[String]](consumerActor))
+        .map(_.message)
+        .map { v: String =>
+          v.toUpperCase
+        }
+        .toMat(TestSink.probe[String])(Keep.right)
+        .run()
 
-      sinkProbe.request(n = 2)
+    sinkProbe.request(n = 2)
 
-      Thread.sleep(60 * 1000)  // ample time for things to fail if they are meant to
+    Thread.sleep(60 * 1000)  // ample time for things to fail if they are meant to
 
-      EmbeddedKafka.startKafka(kafkaDir)
-      Thread.sleep(5000)  // let Kafka start up
+    EmbeddedKafka.startKafka(kafkaDir)
+    Thread.sleep(5 * 1000)  // let Kafka start up
 
-      sinkProbe.expectNext("A", "B")
-    } finally {
-      EmbeddedKafka.stopKafka()
-      EmbeddedKafka.stopZooKeeper()
-    }
+    sinkProbe.expectNext("A", "B")
+  }
+
+  it should
+    "be resilient to Kafka becoming unavailable during processing and resume processing as soon as Kafka becomes available" in {
+    val zkDir = testZooKeeperDir()
+    val kafkaDir = testKafkaDir()
+    EmbeddedKafka.startZooKeeper(zkDir)
+    EmbeddedKafka.startKafka(kafkaDir)
+
+    // let ZooKeeper/Kafka start up - otherwise publishStringMessageToKafka() may throw net.manub.embeddedkafka.KafkaUnavailableException
+    Thread.sleep(5 * 1000)
+
+    val topic = testTopic()
+
+    publishStringMessageToKafka(topic, "a")
+    publishStringMessageToKafka(topic, "b")
+    publishStringMessageToKafka(topic, "c")
+
+    val kafka = new ReactiveKafka()
+
+    val consumerProperties =
+      ConsumerProperties(
+        brokerList = brokerList,
+        zooKeeperHost = zooKeeperHost,
+        topic = topic,
+        groupId = testConsumerGroupId(),
+        decoder = new StringDecoder())
+    val consumerActor = system.actorOf(
+      kafka.consumerActorProps(consumerProperties),
+      testSourceName())
+
+    val sinkProbe =
+      Source(ActorPublisher[KafkaMessage[String]](consumerActor))
+        .map(_.message)
+        .map { v: String =>
+          v.toUpperCase
+        }
+        .toMat(TestSink.probe[String])(Keep.right)
+        .run()
+
+    sinkProbe.request(n = 2)
+    sinkProbe.expectNext("A", "B")
+
+    EmbeddedKafka.stopKafka()
+
+    sinkProbe.request(n = 1)
+    Thread.sleep(60 * 1000)  // ample time for things to fail if they are meant to
+
+    EmbeddedKafka.startKafka(kafkaDir)
+    Thread.sleep(5 * 1000)  // let Kafka start up
+
+    sinkProbe.expectNext("C")
   }
 
   def testClientId() = s"clientId-${randomUUID()}"
